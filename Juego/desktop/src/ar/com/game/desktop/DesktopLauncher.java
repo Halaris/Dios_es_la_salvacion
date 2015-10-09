e92fe342b057c285e38841a95391bfa0a66a7188
damase46 <sergio_nod@hotmail.com>
2015-10-09 15:22:38 -0300

    prueba_0.1

---
+++ Juego/desktop/src/ar/com/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ar.com.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ar.com.game.DiosSalvacion;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiosSalvacion(), config);
+	}
+}
